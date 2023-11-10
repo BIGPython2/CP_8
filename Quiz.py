8304444979d18949707d459f05c25c2cb357fa31
BIGPython2 <addpac3@gmail.com>
2023-11-10 20:49:36 +0900

    로또 번호 생성

---
+++ Quiz.py
@@ -0,0 +1,13 @@
+import random
+number = random.randint(1, 45)
+
+example_list = [4,2,1]
+example_list.sort()
+
+if number in number_list:
+print(“number_list에 해당 숫자가 있습니다.”)
+if number not in number_list:
+print(“number_list에 해당 숫자가 없습니다.”)
+
+print("생성된 로또 번호는, number, "입니다")
+
